942e4ae3924ae1458cc059adbd794fba5f20b416
John Walter <johnkwalter@gmail.com>
2023-01-25 14:16:21 -0800

    Started on iterating through all search results pages

--- moto_scrape.py
+++ moto_scrape.py
@@ -27,13 +27,16 @@ TODO: break the received URLS into parts and place the unique post IDs
 
 """
 
-url_test = "https://seattle.craigslist.org/search/mca?purveyor=owner#search=1~list~0~0"
+url_test = "https://seattle.craigslist.org/search/mca#search=1~list~0~0"
 url_ad_list_test = ["https://seattle.craigslist.org/see/mcd/d/seattle-2020-harley-davidson-fat-bob/7582174231.html",
             "https://seattle.craigslist.org/see/mcd/d/kent-2018-kawasaki-z650-stunt-bike/7582217722.html",
             "https://seattle.craigslist.org/see/mcd/d/kent-2021-bmw-m1000rr/7582086314.html",
             ]
 
-citys = ["seattle", "portland", "sfbay"]
+cities = ["seattle", "portland", "sfbay"]
+url_search_list = []
+for city in cities:
+    url_search_list.append('https://' + city + '.craigslist.org/search/mca#search=1~list')
 
 # Brings up a list of search results on Craiglist and scrapes all of the urls of each ad
 def get_list_of_urls(url):
@@ -106,10 +109,22 @@ ad_details_dict = {
 main_df = pd.DataFrame([ad_details_dict])
 
 # Creates a list of urls from a search results page
-url_ad_list = get_list_of_urls(url_test)
+full_url_ad_list = []
+last_page = 1000
+driver.get('https://seattle.craigslist.org/search/mca#search=1~list~' + str(last_page) + '~0')
+time.sleep(2)
+last_page = re.search(r'~(\d+)~', driver.current_url)
+last_page = int(last_page.group(1))
+i = 0
+while i <= last_page:
+    url = 'https://seattle.craigslist.org/search/mca#search=1~list~' + str(i) + '~0'
+    url_ad_list = get_list_of_urls(url)
+    for url in url_ad_list:
+        full_url_ad_list.append(url)
+    i+=1
 
 # Adds a new row to the dataframe for each individual ad
-for url_ad in url_ad_list_test:
+for url_ad in full_url_ad_list:
     time.sleep(random.randint(0,3))
     ad_details = cl_ad_scrape(url_ad)
     new_df = pd.DataFrame([ad_details])
