5c07ad09db27392b175f2c0d40a8553cb8e328dd
John Walter <johnkwalter@gmail.com>
2023-01-26 10:19:15 -0800

    Can now iterate through list of cities to return full list of ads, and functionized the calls

--- moto_scrape.py
+++ moto_scrape.py
@@ -26,18 +26,43 @@ TODO: functionize selenium code
 TODO: handle when driver gets redirected to an already visited page
 TODO: break the received URLS into parts and place the unique post IDs
 
+TODO: Read back list of already captured urls, compare it to newly captured list,
+        and keep only the new urls
+TODO: Incorporate list of cities to iterate through to create big url list
 """
 
+# Url variables to test from
 url_test = "https://seattle.craigslist.org/search/mca#search=1~list~0~0"
 url_ad_list_test = ["https://seattle.craigslist.org/see/mcd/d/seattle-2020-harley-davidson-fat-bob/7582174231.html",
             "https://seattle.craigslist.org/see/mcd/d/kent-2018-kawasaki-z650-stunt-bike/7582217722.html",
             "https://seattle.craigslist.org/see/mcd/d/kent-2021-bmw-m1000rr/7582086314.html",
             ]
 
-cities = ["seattle", "portland", "sfbay"]
-url_search_list = []
-for city in cities:
-    url_search_list.append('https://' + city + '.craigslist.org/search/mca#search=1~list')
+# List of cities that craigslist motorcycle ads will be scraped from
+cities = ["bellingham", "yakima"]
+
+# Create a blank dictionary to initialize a dataframe from
+ad_details_dict = {
+    # 'title' : [],
+    # 'url' : [],
+    # 'price' : [],
+    # 'body' : [],
+    # 'fuel' : [],
+    # 'odometer' : [],
+    # 'street legal' : [],
+    # 'title status' : [],
+    # 'transmission' : [],
+}
+
+# Create a blank dataframe from that blank dictionary
+main_df = pd.DataFrame([ad_details_dict])
+
+# Create a list of urls by list of cities to search craigslist with
+def city_search_url_list(cities):
+    url_search_list = []
+    for city in cities:
+        url_search_list.append('https://' + city + '.craigslist.org/search/mca#search=1~list~')
+    return url_search_list
 
 # Brings up a list of search results on Craiglist and scrapes all of the urls of each ad
 def get_list_of_urls(url):
@@ -53,7 +78,12 @@ def get_list_of_urls(url):
 def cl_ad_scrape(url):
     driver.get(url)
     details_dict = {}
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "postingbody")))
+
+    try:
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "postingbody")))
+    except:
+        return details_dict
+
     try:
         details_dict['title'] = driver.find_element(By.ID, 'titletextonly').text
     except:
@@ -94,42 +124,49 @@ def cl_ad_scrape(url):
 
     return details_dict
 
-# Create a blank dictionary to initialize a dataframe from
-ad_details_dict = {
-    # 'title' : [],
-    # 'url' : [],
-    # 'price' : [],
-    # 'body' : [],
-    # 'fuel' : [],
-    # 'odometer' : [],
-    # 'street legal' : [],
-    # 'title status' : [],
-    # 'transmission' : [],
-}
-
-main_df = pd.DataFrame([ad_details_dict])
-
 # Creates a list of urls from a search results page
-full_url_ad_list = []
-last_page = 1000
-driver.get('https://seattle.craigslist.org/search/mca#search=1~list~' + str(last_page) + '~0')
-time.sleep(2)
-last_page = re.search(r'~(\d+)~', driver.current_url)
-last_page = int(last_page.group(1))
-i = 0
-while i <= last_page:
-    url = 'https://seattle.craigslist.org/search/mca#search=1~list~' + str(i) + '~0'
-    url_ad_list = get_list_of_urls(url)
-    for url in url_ad_list:
-        full_url_ad_list.append(url)
-    i+=1
+def get_url_ad_list(base_url):
+    full_url_ad_list = []
+    last_page = 1000
+    driver.get(base_url + str(last_page) + '~0')
+    time.sleep(2)
+    last_page = re.search(r'~(\d+)~', driver.current_url)
+    last_page = int(last_page.group(1))
+    i = 0
+    while i <= last_page:
+        url = base_url + str(i) + '~0'
+        url_ad_list = get_list_of_urls(url)
+        for url in url_ad_list:
+            full_url_ad_list.append(url)
+        i+=1
+    return full_url_ad_list
 
 # Adds a new row to the dataframe for each individual ad
-for url_ad in full_url_ad_list:
-    time.sleep(random.randint(0,3))
-    ad_details = cl_ad_scrape(url_ad)
-    new_df = pd.DataFrame([ad_details])
-    main_df = pd.concat([main_df, new_df], ignore_index=True)
+def add_ad_to_dataframe(full_url_ad_list, main_df):
+    i = 0
+    total = len(full_url_ad_list)
+    for url_ad in full_url_ad_list:
+        time.sleep(random.randint(0,3))
+        ad_details = cl_ad_scrape(url_ad)
+        new_df = pd.DataFrame([ad_details])
+        main_df = pd.concat([main_df, new_df], ignore_index=True)
+        i+=1
+        if i%50 == 0:
+            print(f"{str(i)} of {str(total)} scraped ")
+    return main_df
+
+# Combines all of the city ad lists into one large url list
+def combine_city_url_lists(url_search_list):
+    full_url_ad_list = []
+    for url in url_search_list:
+        url_ad_list = get_url_ad_list(url)
+        for url in url_ad_list:
+            full_url_ad_list.append(url)
+    return full_url_ad_list
+
+url_search_list = city_search_url_list(cities)
+full_url_ad_list = combine_city_url_lists(url_search_list)
+main_df = add_ad_to_dataframe(full_url_ad_list, main_df)
 
 # Creates a csv file with all of the dataframe information
 main_df.to_csv('test_df_to_csv.csv', encoding='utf-8', index=False)
